fba2420b5665d63e05ef3c85577500c14551749a
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-11-11 18:41:58 +0530

    Finish Coin List Screen

---
+++ app/src/main/java/com/rohitjakhar/cryptocoin/presentation/Screen.kt
@@ -0,0 +1,6 @@
+package com.rohitjakhar.cryptocoin.presentation
+
+sealed class Screen(val route: String) {
+    object CoinListScreen : Screen("coin_list_screen")
+    object CoinDetailsScreen : Screen("coin_details_screen")
+}
