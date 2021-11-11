0015bbeab5d4f1ef2d284ce522bd876d9d170363
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-11-11 19:01:12 +0530

    Finish Coin Details Screen

---
+++ app/src/main/java/com/rohitjakhar/cryptocoin/presentation/coin_details/CoinDetailScreen.kt
@@ -0,0 +1,106 @@
+package com.rohitjakhar.cryptocoin.presentation.coin_details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.flowlayout.FlowRow
+import com.rohitjakhar.cryptocoin.presentation.Screen
+import com.rohitjakhar.cryptocoin.presentation.coin_details.component.CoinTag
+import com.rohitjakhar.cryptocoin.presentation.coin_details.component.TeamListItem
+import com.rohitjakhar.cryptocoin.presentation.coin_list.component.CoinListItem
+
+@Composable
+fun CoinListScreen(
+    viewModel: CoinDetailsViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        state.coins?.let { coin ->
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+                            style = MaterialTheme.typography.h2,
+                            modifier = Modifier.weight(8f)
+                        )
+                        Text(
+                            text = if (coin.isActive) "Active" else "Inactive",
+                            color = if (coin.isActive) Color.Green else Color.Red,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .weight(2f)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = coin.description,
+                        style = MaterialTheme.typography.body2
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.h3
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin.tags.forEach { tag ->
+                            CoinTag(tag)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Team Member",
+                        style = MaterialTheme.typography.h3
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                items(coin.team) {teamMember ->
+                    TeamListItem(teamMember, modifier = Modifier.fillMaxWidth().padding(10.dp))
+                    Divider()
+                }
+            }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
---
+++ app/src/main/java/com/rohitjakhar/cryptocoin/presentation/coin_details/component/TeamListItem.kt
@@ -0,0 +1,35 @@
+package com.rohitjakhar.cryptocoin.presentation.coin_details.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.rohitjakhar.cryptocoin.data.remote.dto.CoinDetailsDto
+
+@Composable
+fun TeamListItem(
+    teamMember: CoinDetailsDto.TeamMember,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = teamMember.name,
+            style = MaterialTheme.typography.h4,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = teamMember.position,
+            style = MaterialTheme.typography.body2,
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
